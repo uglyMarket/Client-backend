20988704c8503e7824a5c5efa0cd18d5fbe87e29
sion <topaegr3525@kakao.com>
2024-07-22 18:00:43 +0900

    init commit

---
+++ src/main/java/com/sparta/uglymarket/UglyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.uglymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UglyMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UglyMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/uglymarket/UglyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.uglymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UglyMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
