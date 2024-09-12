f08e776dc0aedf949a6d03db7cb7a1e6333a6c71
sion <topaegr3525@kakao.com>
2024-09-12 21:34:04 +0900

    refactor : product 도메인계층과 서비스 계층으로 나누어 재설계

--- src/main/java/com/sparta/uglymarket/product/service/ProductService.java
+++ src/main/java/com/sparta/uglymarket/product/service/ProductService.java
@@ -2,14 +2,12 @@ package com.sparta.uglymarket.product.service;
 
 import com.sparta.uglymarket.exception.CustomException;
 import com.sparta.uglymarket.exception.ErrorMsg;
-import com.sparta.uglymarket.product.dto.GetAllProductsResponse;
-import com.sparta.uglymarket.product.dto.ProductResponse;
+import com.sparta.uglymarket.product.domain.ProductDomain;
 import com.sparta.uglymarket.product.entity.Product;
+import com.sparta.uglymarket.product.mapper.ProductMapper;
 import com.sparta.uglymarket.product.repository.ProductRepository;
-import com.sparta.uglymarket.review.dto.ReviewResponse;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,39 +21,26 @@ public class ProductService {
 
 
     //전체 상품 조회
-    public List<GetAllProductsResponse> getAllProducts() {
+    public List<ProductDomain> getAllProducts() {
 
-        //전체 상품 조회
+        // 모든 상품을 엔티티로 조회
         List<Product> products = productRepository.findAll();
-        return convertToGetAllProductsResponseList(products);
+
+        // 엔티티 리스트 -> 도메인 리스트로 변환
+        return ProductMapper.toDomainList(products);
+
+
     }
 
-    //특정 상품 세부조회
-    public ProductResponse getProductById(long productId) {
+    // 특정 상품 조회
+    public ProductDomain getProductById(long productId) {
 
-        //특정 상품 조회
+        // 특정 상품을 엔티티로 조회
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new CustomException(ErrorMsg.PRODUCT_NOT_FOUND));
 
-        return convertToProductResponse(product);
-    }
-
-    //DTO 변환 메서드
-    private List<GetAllProductsResponse> convertToGetAllProductsResponseList(List<Product> products) {
-        List<GetAllProductsResponse> responseList = new ArrayList<>();
-        for (Product product : products) {
-            responseList.add(new GetAllProductsResponse(product));
-        }
-        return responseList;
-    }
-
-    ///DTO 변환 메서드, 상품에 달려있는 리뷰들 조회
-    private ProductResponse convertToProductResponse(Product product) {
-        List<ReviewResponse> reviews = new ArrayList<>();
-        for (var review : product.getReviews()) {
-            reviews.add(new ReviewResponse(review));
-        }
-        return new ProductResponse(product, reviews);
+        // 도메인 객체로 변환
+        return ProductMapper.toDomain(product);
     }
 
 }
